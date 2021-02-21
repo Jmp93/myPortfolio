6eb168bf31043203d23600bb39d3effe3c303523
James Pointer <jmp.point@gmail.com>
2021-02-21 17:56:11 +0000

    inline js test

--- dist/app.js
+++
@@ -1,6 +0,0 @@
-const burger = document.querySelector('.mobile');
-const nav = document.querySelector('.sidebar');
-
-burger.addEventListener('click', () => {
-  nav.classList.toggle('active');
-});
